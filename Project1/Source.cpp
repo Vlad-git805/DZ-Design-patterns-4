6c957ca11c0726621539f973c6157b58e4b16f50
Vlad805 <zhomyruk_ak18@nuwm.edu.ua>
2020-09-07 20:20:12 +0300

    first commit

---
+++ Project1/Source.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <fstream>
+#include <atltime.h>
+using namespace std;
+
+class Database
+{
+private:
+	const string connectionString = "database.txt";
+	bool isConnected = false;
+
+public:
+
+	void Log(string way)
+	{
+		SYSTEMTIME st;
+		GetSystemTime(&st);
+		ofstream fout;
+		fout.open(way, ios::app);
+		string str;
+		cout << "Enter message" << endl;
+		cin >> str;
+		fout << str<<"  ";
+		fout << to_string(st.wHour) + ":" + to_string(st.wMinute) + " / " + to_string(st.wDay) + "." + to_string(st.wMonth) + "." + to_string(st.wYear) << endl;
+		fout.close();
+	}
+
+	bool IsConnect(string str)
+	{
+		if (str == connectionString)
+		{
+			return (rand() % 10) < 6;
+		}
+		else return false;
+	}
+
+};
+
+class DatabaseProxy 
+{
+private:
+	Database * _database = new Database();
+	const string _connectionString = "localTempDatabase.txt";
+public:
+	void Log()
+	{
+		if (_database == nullptr)
+		{
+			_database = new Database();
+		}
+
+		if (_database->IsConnect("database.txt") == false)
+		{
+			_database->Log("localTempDatabase.txt");
+		}
+		else
+		{
+			bool is_clear = false;
+			ifstream f;
+			f.open(_connectionString);
+			if (f)
+			{
+				char c;
+				if (!(f.get(c)))
+				{
+					f.close();
+				}
+				else
+				{
+					f.close();
+					f.open(_connectionString);
+					ofstream fout;
+					fout.open("database.txt", ios::app);
+					string a;
+					while (!f.eof())
+					{
+						getline(f, a);
+						if (!f.eof())
+						{
+							fout << a << endl;
+						}
+					}
+					fout.close();
+				}
+			}
+			f.close();
+			ofstream ofs;
+			ofs.open(_connectionString);
+			ofs.close();
+			_database->Log("database.txt");
+		}
+	}
+};
+
+int main()
+{
+	srand(time(0));
+
+	DatabaseProxy* proxy = new DatabaseProxy();
+
+	proxy->Log();
+
+	system("pause");
+	return 0;
+}
